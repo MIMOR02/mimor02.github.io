470cb3965e467a85e7d4c34dc870f3c9523795c4
MIMOR02 <totoroninja123@gmail.com>
2021-04-21 19:29:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -90,7 +90,7 @@ function render(grid,cols,rows,applyRule){
 				rect(x,y,resolution-1,resolution-1);
 			}
             else if(cell.getState()==3){
-                fill(0,0,0);
+                fill(100,100,100);
                 stroke(0);
                 rect(x,y,resolution-1,resolution-1);
             }
